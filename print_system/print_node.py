fe5fd1f42ad04cb7306bae71a8f50470cc070ab5
jmHwang <wjdals074@gmail.com>
2024-12-12 14:35:26 +0900

    fix : ROS 매니저 시스템, 프린터 시스템 수정

---
+++ print_system/print_node.py
@@ -0,0 +1,83 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from control_msgs.msg import RobotRecevieMoving
+from print import Printer  # printer 모듈 가져오기
+
+class PrintNode(Node):
+    def __init__(self):
+        super().__init__('print_node')  # 노드 이름
+        self.isActive = False
+        self.user_name = ""
+        self.file_path = ""
+     
+        # 발행 - 스토리지 이동 요청
+        self.publisher_request_move= self.create_publisher(RobotRecevieMoving, '/moving_receive', 10) 
+        
+        # 구독 - 인쇄 시작 요청( = 스토리지 프린터기로 도착 했을때 TaskManger에서 발행) 
+        self.subscription_manager = self.create_subscription(RobotRecevieMoving,'/print_request',  self.listener_print_callback, 10)  # 큐 크기       
+        self.subscription_manager
+
+        # 발행 - 프린터 인쇄 완료
+        self.publisher_print_complete = self.create_publisher(String, '/print_complete', 10) 
+
+       
+              
+
+    def send_request_print_callback(self, user_name, file_path):       
+        self.isActive = True
+        self.user_name = user_name
+        self.file_path = file_path
+
+        # 1. 스토리지 프런터기로 이동 요청 - ROS 발행
+        msg = RobotRecevieMoving()
+        msg.request_system = 'print'  # 메시지 내용
+        msg.user_name = user_name  # 메시지 내용
+        msg.file_path= file_path
+        self.publisher_request_move.publish(msg)        
+        self.get_logger().info('Publishing: "%s"' % msg.request_system)
+
+        
+
+    def send_print_callback(self, result_msg):       
+        msg = String()
+        msg.data = result_msg  # 메시지 내용  
+        self.publisher_print.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+
+
+    #스토리지 프린터기 앞 도착 완료(출력 요청) - ROS구독 (매니저->프린터시스템)
+    def listener_print_callback(self, msg):
+         # 구독한 메시지를 출력        
+        self.get_logger().info('I heard : "%s", User Name: "%s", file_path: %s' % (msg.request_system, msg.user_name, msg.file_path))
+
+        # 1. 프린터 인쇄 요청
+        printer = Printer()# 프린터 인스턴스 생성
+        result_msg = printer.print_file(msg.file_path)  # 프린터 모듈 사용
+        
+
+        # 4. 프린터 인쇄 결과 ROS 발행 - ROS 발행(프린터->매니저)
+        send_msg = String()
+        if '완료' in result_msg :            
+            send_msg.data = "complete" 
+        else: 
+            send_msg.data = "error" 
+      
+        self.publisher_print_complete.publish(send_msg)
+        self.get_logger().info('Publishing publisher_print_complete: "%s"' % send_msg.data)   
+        self.isActive = False   
+
+def main(args=None):
+    rclpy.init(args=args)  # ROS 2 초기화
+    print_node = PrintNode()
+
+    try:
+        rclpy.spin(print_node)  # 노드 실행
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print_node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- print_system/print_server.py
+++ print_system/print_server.py
@@ -1,20 +1,15 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS  # flask-cors 모듈 가져오기
 import os
-from print import print_file  # printer 모듈 가져오기
 import rclpy
-from rclpy.node import Node
 from print_node import PrintNode
 
 app = Flask(__name__)
 CORS(app)  # 모든 도메인에서의 접근을 허용합니다.
 
+# ROS 초기화
 rclpy.init()
-node = PrintNode() #ROS 발행
-
-######################################################
-# 테스트용 - 미사용 코드
-######################################################
+node = PrintNode()  # ROS 발행
 
 # 저장할 디렉토리 설정
 UPLOAD_FOLDER = './print_system/request_print'
@@ -33,36 +28,27 @@ def request_print():
         return jsonify({"error": "No selected file"}), 400
 
     if not user:
-         return jsonify({"error": "user is required"}), 400
+        return jsonify({"error": "User is required"}), 400
 
     # 파일 저장
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(file_path)  # 파일 저장
-    print("file_path"+file_path)
-    print("user :"+user)
+    print("File saved at:", file_path)
+    print("User:", user)
+
     try:
-        # 프린터에 파일 출력
-        #job_id = print_file(user, file_path)  # 프린터 모듈 사용
-        
-        #발행 - 스토리지 프린터기로 이동 요청
-        node.send_move_callback(user)
-
-
-        # 프린트 하기
-        msg = print_file(file_path)  # 프린터 모듈 사용    
+        # ROS 발행 - 프린터 노드로 인쇄 요청(+ 스토리지 이동 포함)   
+        node.send_request_print_callback(user, file_path)
 
 
         return jsonify({"success": True, "file_path": file_path, "user": user}), 200 
 
     except ValueError as e:
         print(jsonify({"error": str(e)}), 404)
-        msg = "error : 404"
         return jsonify({"error": str(e)}), 404 
     except Exception as e:
         print(jsonify({"error": f"An error occurred: {str(e)}"}), 500)
-        msg = "An error occurred: 500"
         return jsonify({"error": f"An error occurred: {str(e)}"}), 500 
 
 if __name__ == "__main__":
-     #app.run(host="0.0.0.0", port=5000)  # 서버의 IP 주소로 변경
-     app.run(host="192.168.0.24", port=5000)
+    app.run(host="192.168.0.24", port=5000)  # 서버의 IP 주소로 변경
