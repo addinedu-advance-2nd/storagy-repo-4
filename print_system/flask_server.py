2dae2902fe51886a234261a88c242663996288f8
jmHwang <wjdals074@gmail.com>
2024-12-13 18:03:39 +0900

    fix : 프린트 서버 네임 변경

--- print_system/flask_server.py
+++ print_system/flask_server.py
@@ -23,10 +23,8 @@ def request_print():
 
     file = request.files['file']  # 클라이언트에서 업로드한 파일
     user = request.form.get('user')  # 클라이언트에서 프린터 이름을 받음
-
-    if file.filename == '':
-        return jsonify({"error": "No selected file"}), 400
-
+    user_id = request.form.get('user_id')  # 챗봇 사용자 id
+  
     if not user:
         return jsonify({"error": "User is required"}), 400
 
@@ -35,6 +33,7 @@ def request_print():
     file.save(file_path)  # 파일 저장
     print("File saved at:", file_path)
     print("User:", user)
+    print("User ID:", user_id)
 
     try:
         # ROS 발행 - 프린터 노드로 인쇄 요청(+ 스토리지 이동 포함)   
@@ -49,6 +48,54 @@ def request_print():
     except Exception as e:
         print(jsonify({"error": f"An error occurred: {str(e)}"}), 500)
         return jsonify({"error": f"An error occurred: {str(e)}"}), 500 
+
+
+@app.route("/request_cancel", methods=["POST"])
+def request_print():
+    
+    user = request.form.get('user')  # 클라이언트에서 프린터 이름을 받음
+    
+    msg = request.form.get('msg')  # 클라이언트에서 프린터 이름을 받음
+
+    if not user:
+        return jsonify({"error": "User is required"}), 400
+
+   
+    print("User:", user)
+
+    try:
+        
+
+        return jsonify({"success": True, "user": user, "msg": msg}), 200 
+
+    except ValueError as e:
+        print(jsonify({"error": str(e)}), 404)
+        return jsonify({"error": str(e)}), 404 
+    except Exception as e:
+        print(jsonify({"error": f"An error occurred: {str(e)}"}), 500)
+        return jsonify({"error": f"An error occurred: {str(e)}"}), 500 
+
+@app.route("/request_origin", methods=["POST"])
+def request_print():
+    
+    user = request.form.get('user')  # 클라이언트에서 프린터 이름을 받음
+ 
+    if not user:
+        return jsonify({"error": "User is required"}), 400
+
+   
+    print("User:", user)
+
+    try:     
+        return jsonify({"success": True, "user": user, "msg": msg}), 200 
+
+    except ValueError as e:
+        print(jsonify({"error": str(e)}), 404)
+        return jsonify({"error": str(e)}), 404 
+    except Exception as e:
+        print(jsonify({"error": f"An error occurred: {str(e)}"}), 500)
+        return jsonify({"error": f"An error occurred: {str(e)}"}), 500 
+
 
 if __name__ == "__main__":
     app.run(host="192.168.0.24", port=5000)  # 서버의 IP 주소로 변경
