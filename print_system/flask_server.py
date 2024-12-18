42addcbaa337843233b12cb626f8a781abdd5b87
jmHwang <wjdals074@gmail.com>
2024-12-18 10:48:17 +0900

    fixi flas_server.py

--- print_system/flask_server.py
+++ print_system/flask_server.py
@@ -50,52 +50,6 @@ def request_print():
         return jsonify({"error": f"An error occurred: {str(e)}"}), 500 
 
 
-@app.route("/request_cancel", methods=["POST"])
-def request_print():
-    
-    user = request.form.get('user')  # 클라이언트에서 프린터 이름을 받음
-    
-    msg = request.form.get('msg')  # 클라이언트에서 프린터 이름을 받음
-
-    if not user:
-        return jsonify({"error": "User is required"}), 400
-
-   
-    print("User:", user)
-
-    try:
-        
-
-        return jsonify({"success": True, "user": user, "msg": msg}), 200 
-
-    except ValueError as e:
-        print(jsonify({"error": str(e)}), 404)
-        return jsonify({"error": str(e)}), 404 
-    except Exception as e:
-        print(jsonify({"error": f"An error occurred: {str(e)}"}), 500)
-        return jsonify({"error": f"An error occurred: {str(e)}"}), 500 
-
-@app.route("/request_origin", methods=["POST"])
-def request_print():
-    
-    user = request.form.get('user')  # 클라이언트에서 프린터 이름을 받음
- 
-    if not user:
-        return jsonify({"error": "User is required"}), 400
-
-   
-    print("User:", user)
-
-    try:     
-        return jsonify({"success": True, "user": user, "msg": msg}), 200 
-
-    except ValueError as e:
-        print(jsonify({"error": str(e)}), 404)
-        return jsonify({"error": str(e)}), 404 
-    except Exception as e:
-        print(jsonify({"error": f"An error occurred: {str(e)}"}), 500)
-        return jsonify({"error": f"An error occurred: {str(e)}"}), 500 
-
 
 if __name__ == "__main__":
     app.run(host="192.168.0.24", port=5000)  # 서버의 IP 주소로 변경
